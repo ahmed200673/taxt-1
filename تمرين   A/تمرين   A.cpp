aefa403441ee0da9a1b8d186761c23d3e319a9fb
ahmed200673 <ahmedabdalrhem918@gmail.com>
2025-01-03 08:19:36 +0300

    Add files via upload

---
+++ تمرين   A/تمرين   A.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream >
+#include <math.h>
+using namespace std;
+int main() {
+
+	double x, y, z;
+	char sign, sign2;
+	cout << " enter the equation :" << endl;
+	cin >> x >> sign >> y >> sign2 >> z;
+
+		 if (sign == '+' && sign2 == '+') { cout << x << "+" << y << "+" << z << "=" << z + y + z << endl; }//1
+
+			else if (sign == '-' && sign2 == '-') { cout << x << "-" << y << "-" << z << "=" << z - y - z << endl; }//1
+
+				else if (sign == '*' && sign2 == '*') { cout << x << "*" << y << "*" << z << "=" << z * y * z << endl; }//1
+	
+					else if (sign == '/' && sign2 == '/') { cout << x << "/" << y << "/" << z << "=" << z / y / z << endl; }//1
+
+						else if (sign == '+' && sign2 == '-') { cout << x << "+" << y << "-" << z << "=" << z + y - z << endl; }//1
+
+							else if (sign == '-' && sign2 == '+') { cout << x << "-" << y << "+" << z << "=" << z - y + z << endl; }//1
+
+								else if (sign == '+' && sign2 == '*') { cout << x << "+" << y << "*" << z << "=" << z + y * z << endl; }//1
+
+									else if (sign == '*' && sign2 == '+') { cout << x << "*" << y << "+" << z << "=" << z * y + z << endl; }//1
+
+								else if (sign == '*' && sign2 == '/') { cout << x << "*" << y << "/" << z << "=" << z * y / z << endl; }//1
+
+							else if (sign == '/' && sign2 == '*') { cout << x << "/" << y << "*" << z << "=" << z / y * z << endl; }//1
+
+						else if (sign == '*' && sign2 == '-') { cout << x << "*" << y << "-" << z << "=" << z * y - z << endl; }//1
+
+					else if (sign == '-' && sign2 == '*') { cout << x << "-" << y << "*" << z << "=" << z - y * z << endl; }//1
+
+				else if (sign == '+' && sign2 == '/') { cout << x << "+" << y << "/" << z << "=" << z + y / z << endl; }//1
+
+			else if (sign == '/' && sign2 == '+') { cout << x << "/" << y << "+" << z << "=" << z / y + z << endl; }//1
+
+		else if (sign == '/' && sign2 == '-') { cout << x << "/" << y << "-" << z << "=" << z / y - z << endl; }//1
+
+	else if (sign == '-' && sign2 == '/') { cout << x << "-" << y << "/" << z << "=" << z - y / z << endl; }//1
+
+		 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+}

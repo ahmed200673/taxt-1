73c07eeaf6fa71321f2b528c69d65a7a0b51384a
ahmed200673 <ahmedabdalrhem918@gmail.com>
2025-01-22 15:22:11 +0300

    Update تمرين   A.cpp

--- تمرين   A/تمرين   A.cpp
+++ تمرين   A/تمرين   A.cpp
@@ -8,37 +8,37 @@ int main() {
 	cout << " enter the equation :" << endl;
 	cin >> x >> sign >> y >> sign2 >> z;
 
-		 if (sign == '+' && sign2 == '+') { cout << x << "+" << y << "+" << z << "=" << z + y + z << endl; }//1
+		 if (sign == '+' && sign2 == '+') { cout << x << "+" << y << "+" << z << "=" << x + y + z << endl; }//1
 
-			else if (sign == '-' && sign2 == '-') { cout << x << "-" << y << "-" << z << "=" << z - y - z << endl; }//1
+			else if (sign == '-' && sign2 == '-') { cout << x << "-" << y << "-" << z << "=" << x - y - z << endl; }//1
 
-				else if (sign == '*' && sign2 == '*') { cout << x << "*" << y << "*" << z << "=" << z * y * z << endl; }//1
+				else if (sign == '*' && sign2 == '*') { cout << x << "*" << y << "*" << z << "=" << x * y * z << endl; }//1
 	
-					else if (sign == '/' && sign2 == '/') { cout << x << "/" << y << "/" << z << "=" << z / y / z << endl; }//1
+					else if (sign == '/' && sign2 == '/') { cout << x << "/" << y << "/" << z << "=" << x / y / z << endl; }//1
 
-						else if (sign == '+' && sign2 == '-') { cout << x << "+" << y << "-" << z << "=" << z + y - z << endl; }//1
+						else if (sign == '+' && sign2 == '-') { cout << x << "+" << y << "-" << z << "=" <<x + y - z << endl; }//1
 
-							else if (sign == '-' && sign2 == '+') { cout << x << "-" << y << "+" << z << "=" << z - y + z << endl; }//1
+							else if (sign == '-' && sign2 == '+') { cout << x << "-" << y << "+" << z << "=" << x - y + z << endl; }//1
 
-								else if (sign == '+' && sign2 == '*') { cout << x << "+" << y << "*" << z << "=" << z + y * z << endl; }//1
+								else if (sign == '+' && sign2 == '*') { cout << x << "+" << y << "*" << z << "=" << x+ y * z << endl; }//1
 
-									else if (sign == '*' && sign2 == '+') { cout << x << "*" << y << "+" << z << "=" << z * y + z << endl; }//1
+									else if (sign == '*' && sign2 == '+') { cout << x << "*" << y << "+" << z << "=" << x * y + z << endl; }//1
 
-								else if (sign == '*' && sign2 == '/') { cout << x << "*" << y << "/" << z << "=" << z * y / z << endl; }//1
+								else if (sign == '*' && sign2 == '/') { cout << x << "*" << y << "/" << z << "=" << x * y / z << endl; }//1
 
 							else if (sign == '/' && sign2 == '*') { cout << x << "/" << y << "*" << z << "=" << z / y * z << endl; }//1
 
 						else if (sign == '*' && sign2 == '-') { cout << x << "*" << y << "-" << z << "=" << z * y - z << endl; }//1
 
-					else if (sign == '-' && sign2 == '*') { cout << x << "-" << y << "*" << z << "=" << z - y * z << endl; }//1
+					else if (sign == '-' && sign2 == '*') { cout << x << "-" << y << "*" << z << "=" << x - y * z << endl; }//1
 
-				else if (sign == '+' && sign2 == '/') { cout << x << "+" << y << "/" << z << "=" << z + y / z << endl; }//1
+				else if (sign == '+' && sign2 == '/') { cout << x << "+" << y << "/" << z << "=" << x + y / z << endl; }//1
 
-			else if (sign == '/' && sign2 == '+') { cout << x << "/" << y << "+" << z << "=" << z / y + z << endl; }//1
+			else if (sign == '/' && sign2 == '+') { cout << x << "/" << y << "+" << z << "=" <<x / y + z << endl; }//1
 
-		else if (sign == '/' && sign2 == '-') { cout << x << "/" << y << "-" << z << "=" << z / y - z << endl; }//1
+		else if (sign == '/' && sign2 == '-') { cout << x << "/" << y << "-" << z << "=" << x / y - z << endl; }//1
 
-	else if (sign == '-' && sign2 == '/') { cout << x << "-" << y << "/" << z << "=" << z - y / z << endl; }//1
+	else if (sign == '-' && sign2 == '/') { cout << x << "-" << y << "/" << z << "=" << x - y / z << endl; }//1
 
 		 
 
